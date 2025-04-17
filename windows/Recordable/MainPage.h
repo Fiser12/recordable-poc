4ff37a0879d08feae3014bcd840a2fef333aa382
Rubén <ruben@nexolabs.xyz>
2025-04-17 16:51:56 +0200

    feat: added windows compatibility, removed expo

---
+++ windows/Recordable/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Recordable::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Recordable::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
